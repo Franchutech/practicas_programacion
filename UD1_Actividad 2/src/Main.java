5f73b7fe54855eb113088dfdffbf4011e52acc4e
COMPUS029\daw1 <srojasc2501@educantabria.es>
2025-10-08 09:50:06 +0200

    UD1_Actividad2: Ejercicios hasta el 9

--- UD1_Actividad 2/src/Main.java
+++ UD1_Actividad 2/src/Main.java
@@ -26,42 +26,49 @@ mayor de edad” o el mensaje de “eres menor de edad”.  */
         int edad2 = lector.nextInt();
         if (edad2 >= 18) {
             System.out.println("Usted es mayor de edad");
-        } else {
+        } else if(edad2 <18 && edad2 >= 0) {
             System.out.println("Eres menor de edad");
         }
+        else{
+            System.out.println("La edad no tiene sentido");
+        }
+
     /*Ejercicio3: Realiza un programa que muestre por pantalla los 20 primeros números naturales (1, 2,
 3... 20). */
-        System.out.println("P3.Ejercicio 3");
+        System.out.println("\nP3.Ejercicio 3");
         System.out.println("Introduce un número máximo:");
         lector = new Scanner(System.in);
         int max;
         max = lector.nextInt();
         for (int i = 1; i <= max; i++) {
-            System.out.println(i);
+            System.out.print(i + ", ");
         }
     /* Ejercicio 4: Realiza un programa que muestre los números pares comprendidos entre el 1 y el 200.
 Para ello utiliza un contador y suma de 2 en 2.
     */
-        System.out.println("P3.Ejercicio 4");
-        for (int n = 2; n <= 200; n += 2){
-            System.out.println(n);
+        System.out.println("\nP3.Ejercicio 4");
+        for (int i = 2; i <= 200; i += 2){
+            System.out.print(i + ", ");
         }
     /*Ejercicio 5: Realiza un programa que muestre los números pares comprendidos entre el 1 y el 200.
 Esta vez utiliza un contador sumando de 1 en 1.
     */
-        System.out.println("P3.Ejercicio 5");
-        for (int m = 1; m <= 200; m += 1){
-            System.out.println(m);
+        System.out.println("\nP3.Ejercicio 5");
+
+            for (int i = 1; i <= 200; i++) {
+                if(i % 2 == 0){
+                System.out.print(i + ", ");
+            }
         }
     /*Ejercicio 6: Realiza un programa que muestre los números desde el 1 hasta un número N que se
         introducirá por teclado.*/
-        System.out.println("P3.Ejercicio 6");
+        System.out.println("\nP3.Ejercicio 6");
         System.out.println("Introduce un número:");
         lector = new Scanner(System.in);
         int maxN;
         maxN= lector.nextInt();
-        for (int f = 1; f <= maxN; f++){
-            System.out.println(f);
+        for (int i = 1; i <= maxN; i++){
+            System.out.print(i + ", ");
         }
     /* Ejercicio 7: Escribe un programa que lea una calificación numérica entre 0 y 10 y la transforma en
 calificación alfabética, escribiendo el resultado.
@@ -75,7 +82,66 @@ calificación alfabética, escribiendo el resultado.
         lector = new Scanner(System.in);
         double nota;
         nota = lector.nextDouble();
-        
+        if(nota < 0.0 || nota > 10.0){
+            System.out.println ("Valor no definido" + nota);
+        }
+        if(nota >= 0 && nota < 3){
+            System.out.println("Muy Deficiente");
+        }
+        if(nota >=3 && nota < 5){
+            System.out.println("Insuficiente");
+        }
+        if(nota >=5 && nota < 6){
+            System.out.println("Bien");
+        }
+        if(nota >=6 && nota <9){
+            System.out.println("Notable");
+        }
+        if(nota >= 9 && nota <= 10){
+            System.out.println("Sobresaliente");
+        }
+    /*Ejercicio 8: Realiza un programa que lea un número positivo N y calcule y visualice su factorial N!
+Siendo el factorial:
+• 0! = 1
+• 1! = 1
+• 2! = 2 * 1
+• 3! = 3 * 2* 1
+• N! = N * (N-1) * (N-2)........* 3*2*1 */
+        System.out.println("\nEjercicio 8");
+        System.out.println("Introduce un número entero para calcular su factorial:");
+        lector = new Scanner(System.in);
+        int num;
+        int factorial = 1;
+        num = lector.nextInt();
+        for (int i = 1; i <= num; i++){
+            factorial = factorial * i;
+        }
+    System.out.println ("El factorial es:" + factorial);
+    /*
+    Escribe un programa que recibe como datos de entrada una hora expresada en horas,
+minutos y segundos que nos calcula y escribe la hora, minutos y segundos que serán,
+transcurrido un segundo.
+     */
+        lector = new Scanner(System.in);
+
+        System.out.println("\nEjercicio 9");
+        System.out.println("Introduce la hora:");
+        int hora;
+        hora = lector.nextInt();
+        System.out.println("Introduce los minutos:");
+        int min;
+        min = lector.nextInt();
+        System.out.println("Introduce los segundos:");
+        int segundos;
+        segundos = lector.nextInt();
+
+        int segundo = 1;
+        int time = hora + min + segundos;
+
+            for (int i = 1; i <= segundo; i++){
+
+        }
+
 
     }
 }
