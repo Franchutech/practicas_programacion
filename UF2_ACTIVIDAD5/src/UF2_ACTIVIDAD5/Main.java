91682c3e32a9b6ac67a1f1f6b8e0387149a20a8a
Francella Rojas C <francellarojasc@gmail.com>
2025-10-21 23:32:36 +0200

    feat: agregando proyecto nuevo UF2_ACTIVIDAD5

---
+++ UF2_ACTIVIDAD5/src/UF2_ACTIVIDAD5/Main.java
@@ -0,0 +1,8 @@
+package UF2_ACTIVIDAD5;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("UF2_ACTIVIDAD5 iniciada correctamente");
+
+    }//void
+}//main
