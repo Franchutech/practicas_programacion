95c41d9cd971dfb47d35dd5d65510027216d56a3
COMPUS029\daw1 <srojasc2501@educantabria.es>
2025-10-23 09:46:51 +0200

    UF2_Actividad5: Ejercicios del 1 al 2

--- UF2_ACTIVIDAD5/src/UF2_ACTIVIDAD5/Main.java
+++ UF2_ACTIVIDAD5/src/UF2_ACTIVIDAD5/Main.java
@@ -23,7 +23,24 @@ palabra de la cadena en una línea distinta.*/
 //            System.out.println(palabras[i]);
 //        }
 
+    /*Ejercicio 2: Crea un programa que pida dos cadenas de texto por teclado y luego indique si son
+iguales, además de si son iguales sin diferenciar entre mayúsculas y minúsculas. */
 
+        in = new Scanner(System.in);
+        System.out.println("A5_E2");
+
+        System.out.println("Introduzca un primer texto:");
+        String textoUno = in.nextLine();
+        System.out.println("Introduzca un segundo texto:");
+        String textoDos = in.nextLine();
+
+        boolean resultado = textoUno.equals(textoDos);
+        System.out.println(resultado);
+
+    /*Ejercicio 3: Crea un programa que pida por teclado tres cadenas de texto: nombre y dos apellidos.
+    Luego mostrará un código de usuario (en mayúsculas) formado por la concatenación de
+    las tres primeras letras de cada uno de ellos. Por ejemplo, si se introduce “Lionel”,
+   Tarazón” y “Alcocer” mostrará “LIOTARALC”. */
 
 
 
