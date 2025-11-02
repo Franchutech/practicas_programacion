9c0085085611381932be9550b76ce49e9825721e
Francella Rojas C <francellarojasc@gmail.com>
2025-11-03 00:40:01 +0100

    UF2_ACT7_Piedra_papel_tijera

---
+++ UF2_ACT7_Francella_Rojas_C/UF2_ACT7_Francella_Rojas_C/src/actividad7/Main.java
@@ -0,0 +1,44 @@
+package actividad7;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.printf("Hello and welcome!");
+
+
+        }
+    }
+
+    /** 1.Haber lo primero que haría sería pues imprimir las instrucciones
+     * 2.Defeniría como hacer un piedra papel tijera, porque creo que tendria que usar,
+     * Strings, para poder usar palabras no se. y definir, que palabra le gana a cual.
+     * Papel le gana a Piedra / piedra le gana a tijera / tijera le gana a papel, nose
+     * si se haría como una especi de mayor que y menor que no se, tal vez dandoles un valor a cada palabra
+     * 3. Crear la jugada, pues creo que puede ser un poco con if con condiciones
+     * pero es que no se si el piedra papel tijera es literal o si se haría definiendo numeros.
+     * porque definiendo numeros sería más facil pienso.
+     * 4. Pedir jugada al usuario, después de que el bucle de la jugada computador se complete.
+     * 5. decidir quién a ganado me imagino tendria que hacerse con un array, que guarde la respuesta
+     * del ordenador, y la respuesta del usuario, y ese array dentro tenga los if que deciden, quién
+     * ganó.
+     * 6. hacer todo en un bucle while, para hacer la jugada repetitiva
+     * 7. las opciones para la jugada pues deberían ser un switch para hacerlo más elegante, pero
+     * mas simple con un println que explique cual letra elegir.*/
+
+    /**Se debe realizar un proyecto para crear el juego de “Piedra, papel o tijera”, el programa debe
+     de seguir la siguiente estructura.
+      Explicarle al jugador cómo se juega.
+      Generar la jugada aleatoria del ordenador.
+      Pedir al jugador su jugada mediante una letra (P para piedra, L para papel, T para tijeras,
+     o S para salir terminando la partida):
+      Decidir quién ha ganado.
+     Una cosa es saber como se juega y otra es saberlo programar, los pasos a seguir serían:
+      Preparar el esqueleto y las constantes necesarias.
+      Mostrar instrucciones al usuario.
+      Generar la jugada del ordenador.
+      Recoger la jugada del usuario.
+      Interpretar la jugada del usuario.
+      Mostrar el resultado de la jugada.
+      Hacer el juego repetitivo.
+     Se admiten y se evaluarán mejoras sustanciales en el programa o en la complejidad del juego. */
