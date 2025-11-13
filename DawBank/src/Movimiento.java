1af4d77087983c3c771d11a67ea32b81226779a7
Francella Rojas C <francellarojasc@gmail.com>
2025-11-14 00:59:13 +0100

    UD3_ACT1_DawBank

--- DawBank/src/Movimiento.java
+++ DawBank/src/Movimiento.java
@@ -2,15 +2,39 @@ import java.time.LocalDateTime;
 
 public class Movimiento {
 
+
     //atributos
 
-    public int id;
-    public LocalDateTime fecha;
-    public String tipo;
-    public double cantidad;
+    private int id;
+    private LocalDateTime fecha;
+    private String tipo;
+    private double cantidad;
 
-//Colección
+    public static int contadorMovimientos = 0;
 
+
+//constructor
+
+    public Movimiento(String tipo, double cantidad){
+        this.tipo = tipo;
+        this.cantidad = cantidad;
+
+        this.id = contadorMovimientos;
+        contadorMovimientos++;
+        fecha = LocalDateTime.now();
+
+    }
+
+    //metodo
+
+    public String mostrarInfoMovimiento(){
+        String mostrarinfo = "";
+        mostrarinfo += "Tipo: " + tipo + "\n";
+        mostrarinfo += "Cantidad: " + cantidad + "\n";
+        mostrarinfo += "ID: " + id + "\n";
+        mostrarinfo += "Fecha: " + fecha + "\n";
+        return mostrarinfo;
+    }
 
 
 } //cierre class movimiento
