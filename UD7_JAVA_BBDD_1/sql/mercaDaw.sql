ca47bba48050fce70e8710cdc37bf85bb62046f2
COMPUS024\daw1 <srojasc2501@educantabria.es>
2026-03-19 09:46:48 +0100

    UD7_JAVA_BBDD_1_Avance2_en clases

--- UD7_JAVA_BBDD_1/sql/mercaDaw.sql
+++ UD7_JAVA_BBDD_1/sql/mercaDaw.sql
@@ -1,6 +1,11 @@
 CREATE DATABASE IF NOT EXISTS mercadaw;
 USE mercadaw;
 
+CREATE TABLE IF NOT EXISTS tipos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre_tipo VARCHAR(50) NOT NULL UNIQUE
+);
+
 CREATE TABLE IF NOT EXISTS productos (
     id INT AUTO_INCREMENT PRIMARY KEY,
     referencia VARCHAR(50) UNIQUE NOT NULL, 
@@ -14,5 +19,24 @@ CREATE TABLE IF NOT EXISTS productos (
     aplicar_dto BOOLEAN DEFAULT FALSE
 );
 
-INSERT INTO productos (referencia, nombre, descripcion, cantidad, precio) 
-VALUES ('REF001', 'Teclado Mecanico', 'RGB Switch Red', 5, 85.50);
+INSERT INTO tipos (nombre_tipo) VALUES 
+('Almacenamiento'), 
+('Monitores'), 
+('Audio'), 
+('Perifericos'), 
+('Hardware'), 
+('Conectores'), 
+('Limpieza'), 
+('Componentes');
+
+INSERT INTO productos (referencia, nombre, descripcion, tipo_id, cantidad, precio) 
+VALUES 
+('REF002', 'Ratón Óptico', 'Inalámbrico 1600 DPI', 4, 15, 25.99),
+('REF003', 'Disco SSD 1TB', 'SSD NVMe ultra rápido', 1, 8, 89.90),
+('REF004', 'Monitor 27"', 'Panel IPS 4K 60Hz', 2, 4, 249.00),
+('REF005', 'Cascos Gaming', 'Sonido 7.1 con micro', 3, 12, 45.00),
+('REF006', 'Memoria RAM 16GB', 'DDR4 3200MHz RGB', 8, 20, 75.25),
+('REF007', 'Tarjeta Gráfica', 'RTX 4060 8GB GDDR6', 8, 3, 320.00),
+('REF008', 'Cable HDMI 2.1', 'Longitud 2 metros 8K', 6, 30, 14.99),
+('REF009', 'Spray Aire Comprimido', 'Limpieza de teclados', 7, 10, 8.50),
+('REF010', 'Placa Base B550', 'Socket AM4 ATX', 5, 2, 125.00);
--- UD7_JAVA_BBDD_1/src/main/java/com/decroly/Inventario.java
+++ UD7_JAVA_BBDD_1/src/main/java/com/decroly/Inventario.java
@@ -4,31 +4,87 @@ import java.sql.Connection;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.sql.PreparedStatement;
+import java.util.Scanner;
+
 
 public class Inventario {
     public static void main(String[] args) {
         System.out.println("Iniciando sistema MercaDaw...");
 
-        String url ="";
-        String user ="";
-        String password ="";
+        String url = "";
+        String user = "";
+        String password = "";
 
         try (BufferedReader br = new BufferedReader(new FileReader("application.dat"))) {
             url = br.readLine();
             user = br.readLine();
             password = br.readLine();
-        }catch (IOException e){
+        } catch (IOException e) {
             System.out.println("Error critico: no se pudo leer el archivo de configuracion");
             return; //sino hay datos puedo seguir
         }
         //PRUEBO LA CONECCION A LA OTRA CLASE.
-        Connection conn = SQLDataBaseManager.getConnection(url,user,password);
-        if (conn !=null) {
+        Connection conn = SQLDataBaseManager.getConnection(url, user, password);
+        if (conn != null) {
             System.out.println("Conexion establecida, puedo trabajar con MercaDaw");
-        }else {
+            probarConexionReal(conn);
+
+            //Aquì si todo funciona bien tendria que ir el menu
+
+            //MENU
+            mostrarMenu();
+
+
+        } else {
             System.out.println("Fallo la conexion. Revisa los datos en application.dat");
             System.out.println("Verifica que MySQL esté encendido y que el usuario/clave sean correctos.");
-            }
+        }
+
+
     }// CIERRE VOID MAIN
+
+
+
+    //METODO PRUEBA DE CONEXIÓN
+
+    public static void probarConexionReal(Connection conn) {
+        String sql = "SELECT COUNT(*) AS total FROM productos";
+
+        try (java.sql.Statement stmt = conn.createStatement();
+             java.sql.ResultSet rs = stmt.executeQuery(sql)) {
+
+            if (rs.next()) {
+                int total = rs.getInt("total");
+                System.out.println(">>> PRUEBA EXITOSA: Hay " + total + " productos en la base de datos.");
+            }
+        } catch (java.sql.SQLException e) {
+            System.out.println(">>> ERROR DE PRUEBA: Conectado pero no puedo leer la tabla: " + e.getMessage());
+        }
+    }//FIN METODO PRUEBA DE CONEXION
+
+    //METODO PARA EL MENU
+
+    public static void mostrarMenu(Scanner input) {
+        System.out.println("\n MENÚ PRINCIPAL ");
+        System.out.println("1. Mostrar todos los productos en el inventario");
+        System.out.println("2. Buscar producto por referencia");
+        System.out.println("3. Buscar producto por tipo");
+        System.out.println("4. Buscar producto por cantidad");
+        System.out.println("5. Insertar un nuevo producto"); //(no permitir referencias repetidas)
+        System.out.println("6. Eliminar producto por referencia");  //referencia no valida
+        System.out.println("7. Actualizar producto"); //(descripción, cantidad, precio, descuento, AplicarDto).
+        System.out.println("8. Insertar un nuevo tipo de producto ");
+        System.out.print("9. Salir.");
+
+        String entrada = input.nextLine().trim();
+        while (!entrada.matches("[1-9]")) {
+            System.out.print("Opción no válida. Ingrese 1-8: ");
+            entrada = input.nextLine().trim();
+        }
+
+        return Integer.parseInt(entrada);
+    }
+
 
 }//CIERRE INVENTARIO
