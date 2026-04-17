e51c334c5508d6bc29fc275a9aeaf150166ba735
Francella Rojas C <francellarojasc@gmail.com>
2026-04-17 06:45:37 +0200

    Arquitectura: Tabla cualidades y datos para el Sombrero

--- Ministerio_Magia_MOM/sql/ministerio_magia_db.sql
+++ Ministerio_Magia_MOM/sql/ministerio_magia_db.sql
@@ -18,3 +18,24 @@ INSERT INTO casas (id, nombre, fundador, animal_representativo, color_primario, 
 (4, 'HUFFLEPUFF', 'Helga Hufflepuff', 'Tejón', 'Yellow', 'Black');
 
 SELECT * FROM casas;
+
+CREATE TABLE cualidades (
+id_cualidad INT PRIMARY KEY, -- este numero es para el usuario (el id de cada cualidad)
+nombre_cualidad VARCHAR(50) NOT NULL UNIQUE,
+id_casa_afinidad INT NOT NULL,
+CONSTRAINT fk_cualidad_casa FOREIGN KEY (id_casa_afinidad) REFERENCES casas(id)
+);
+
+INSERT INTO cualidades (id_cualidad, nombre_cualidad, id_casa_afinidad) VALUES
+(1, 'VALOR', 1),
+(2, 'AMBICION', 2),
+(3, 'INTELIGENCIA', 3),
+(4, 'LEALTAD', 4),
+(5, 'CABALLEROSIDAD', 1),
+(6, 'ASTUCIA', 2),
+(7, 'CURIOSIDAD', 3),
+(8, 'PACIENCIA', 4),
+(9, 'OSADIA', 1),
+(10, 'LIDERAZGO', 2),
+(11, 'CREATIVIDAD', 3),
+(12, 'TRABAJO DURO', 4);
